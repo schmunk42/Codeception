fb4e30967789320aedd0a3a75181db5fb30b57b8
Tobias Munk <schmunk@usrbin.de>
2016-05-11 00:21:44 +0200

    added class and function checks i(test, mocking)

--- shim.php
+++ shim.php
@@ -11,32 +11,40 @@ if (!class_exists('Symfony\Component\CssSelector\CssSelectorConverter')) {
 }
 
 // prefering old names
-
 namespace Codeception\TestCase {
-
-    class Test extends \Codeception\Test\Unit {
-    }
+if (!class_exists('Codeception\TestCase\Unit')) {
+        class Test extends \Codeception\Test\Unit
+        {
+        }
+}
 }
 
 namespace Codeception\Module {
-
-    class Symfony2 extends Symfony {
+if (!class_exists('Codeception\Module\Symfony2')) {
+    class Symfony2 extends Symfony
+    {
     }
 
-    class Phalcon1 extends Phalcon {
+    class Phalcon1 extends Phalcon
+    {
     }
 
-    class Phalcon2 extends Phalcon {
+    class Phalcon2 extends Phalcon
+    {
     }
+}
 }
 
 namespace Codeception\Platform {
+if (!class_exists('Codeception\Platform\Group')) {
     abstract class Group extends \Codeception\GroupObject
     {
     }
+
     abstract class Extension extends \Codeception\Extension
     {
     }
+}
 }
 namespace {
     class_alias('Codeception\TestInterface', 'Codeception\TestCase');
